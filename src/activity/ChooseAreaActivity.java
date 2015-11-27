8a5ab61684077486840176866818374c0f54d0e2
cwh1501413693 <1501413693@qq.com>
2015-11-27 16:08:45 +0800

    First commit.

--- src/activity/ChooseAreaActivity.java
+++ src/activity/ChooseAreaActivity.java
@@ -17,7 +17,10 @@ import model.County;
 import model.Province;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -65,6 +68,13 @@ private int currentLevel;
 @Override
 protected void onCreate(Bundle savedInstanceState) {
 super.onCreate(savedInstanceState);
+SharedPreferences prefs = PreferenceManager. getDefaultSharedPreferences(this);
+if (prefs.getBoolean("city_selected", false)) {
+Intent intent = new Intent(this, WeatherActivity.class);
+startActivity(intent);
+finish();
+return;
+}
 requestWindowFeature(Window.FEATURE_NO_TITLE);
 setContentView(R.layout.choose_area);
 listView = (ListView) findViewById(R.id.list_view);
@@ -82,6 +92,13 @@ queryCities();
 } else if (currentLevel == LEVEL_CITY) {
 selectedCity = cityList.get(index);
 queryCounties();
+} else if (currentLevel == LEVEL_COUNTY) {
+String countyCode = countyList.get(index).getCountyCode();
+Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+intent.putExtra("county_code", countyCode);
+startActivity(intent);
+finish();
+
 }
 }
 });
@@ -179,6 +196,7 @@ response);
 			} else if ("county".equals(type)) {
 			queryCounties();
 			}
+			
 			}
 			});
 			}
